96245bd536934c861fa306a68e604bfaa6f80224
Juliancobos <016200330h@uandina.edu.pe>
2025-10-25 11:15:17 -0500

    normas APA

--- cbs/general.js
+++ cbs/general.js
@@ -1,46 +1,56 @@
-
 $(document).ready(function () {
+  /**
+   * ancho de pantalla
+   */
+  function mostrarAncho() {
+    var ancho = $(window).width();
+    $(".tamano").text(ancho);
+  }
 
-    /**
-     * ancho de pantalla
-     */
-    function mostrarAncho() {
-        var ancho = $(window).width();
-        $('.tamano').text(ancho);
-    }
+  mostrarAncho(); // Mostrar al cargar
 
-    mostrarAncho(); // Mostrar al cargar
+  $(window).on("resize", function () {
+    mostrarAncho(); // Actualizar al redimensionar
+  });
 
-    $(window).on('resize', function () {
-        mostrarAncho(); // Actualizar al redimensionar
-    });
+  /**
+   * cambios en modo oscuro
+   */
+  function ModoOscuro(activo) {
+    $(".onda").toggleClass("oscuro", activo);
+  }
 
-    /**
-     * cambios en modo oscuro
-     */
-    function ModoOscuro(activo) {
-        $('.onda').toggleClass('oscuro', activo);
-    }
+  /**
+   * cambia el <html lang="es" class="dark">
+   */
+  $("#btnoscuro").on("click", function () {
+    $("html").toggleClass("dark");
+    ModoOscuro($("html").hasClass("dark"));
+  });
 
-    /**
-     * cambia el <html lang="es" class="dark">
-     */
-    $('#btnoscuro').on('click', function () {
-        $('html').toggleClass('dark');
-        ModoOscuro($('html').hasClass('dark'));
-    });
-
-    /**
-     * JQuery menu lateral derecho
-     * pequeño
-     */
-    $('#menucontenido').fadeOut('slow');
-    $('#btnmenu').on('click', function () {
-        $('#menucontenido').stop(true, true).slideToggle(300);
-    });
+  /**
+   * JQuery menu lateral derecho
+   * pequeño
+   */
+  $("#menucontenido").fadeOut("slow");
+  $("#btnmenu").on("click", function () {
+    $("#menucontenido").stop(true, true).slideToggle(300);
+  });
 });
 
-
 $(document).ready(function () {
+  $("button").click(function () {
+    // Obtener el destino desde el atributo data-target
+    var destino = $(this).data("target");
+    console.log(destino);
 
+    // Animar el scroll
+    $("html, body").animate(
+      {
+        scrollTop: $(destino).offset().top - 50,
+      },
+      1000,
+      "easeInOutExpo"
+    );
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,14 @@ module.exports = {
     "./cbs/**/*.js"    // JS dentro de cbs/
   ],
   theme: {
-    screens: {
-      sm: '640px',   // sm: aplica desde 640px en adelante
-      md: '768px',   // md: aplica desde 768px en adelante
-      lg: '1024px',  // lg: aplica desde 1024px en adelante
-      xl: '1280px',  // xl: aplica desde 1280px en adelante
-      '2xl': '1536px' // 2xl: aplica desde 1536px en adelante
+    extend: {
+        colors: {
+        testcolor: "#123456",
+      },
     },
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
   ],
-};
 
+};
