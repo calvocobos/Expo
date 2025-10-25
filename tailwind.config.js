3f54a70201c5d0d5a454169a5122959194428aab
Juliancobos <016200330h@uandina.edu.pe>
2025-10-25 15:55:15 -0500

    SEO

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  darkMode: 'class',   // modo oscuro en <html lang="es" class="dark">
+  darkMode: "class", // modo oscuro en <html lang="es" class="dark">
   content: [
-    "./*.html",        // HTML en la raíz
-    "./cbs/**/*.js"    // JS dentro de cbs/
+    "./*.html", // HTML en la raíz
+    "./cbs/**/*.js", // JS dentro de cbs/
   ],
   theme: {
     extend: {
-        colors: {
+      fontFamily: {
+        sans: ["Ubuntu", "sans-serif"], // reemplaza la fuente sans predeterminada
+      },
+      colors: {
         testcolor: "#123456",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
