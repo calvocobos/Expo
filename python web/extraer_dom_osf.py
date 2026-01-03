03e903dfc682607feab3da10ea034c2f459a9709
Juliancobos <016200330h@uandina.edu.pe>
2026-01-03 10:17:29 -0500

    visitas y descargas

---
+++ python web/extraer_dom_osf.py
@@ -0,0 +1,32 @@
+import webview
+import time
+
+URL = "https://osf.io/rqzdx/analytics"
+
+def extraer_dom():
+    # Maximizar ventana
+    window.maximize()
+
+    # esperar a que cargue React/Vue
+    time.sleep(15)
+
+    html = window.evaluate_js("document.documentElement.outerHTML")
+    texto = window.evaluate_js("document.body.innerText")
+
+    with open("extraido_dom_osf.html", "w", encoding="utf-8") as f:
+        f.write(html)
+
+    with open("extraido_txt_osf.txt", "w", encoding="utf-8") as f:
+        f.write(texto)
+
+    window.destroy()
+
+    print("✔  extraido_dom_osf.html creado")
+    print("✔  extraido_txt_osf.txt creado")
+
+window = webview.create_window(
+    title="Repositorio UAC",
+    url=URL
+)
+
+webview.start(extraer_dom)
